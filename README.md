fcc936825458bc234ab12fbffe77ad897f6128c9
Ben Quigley <ben.quigley@protonmail.com>
2019-11-08 15:09:44 -0500

    Add "Hello world" tweeting

--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 This bot creates writing prompts from open source art and a wordlist and
 tweets them out every day for your enjoyment.
 
-On Twitter: https://twitter.com/WritePromptsBot
+On Twitter: [WritePromptsBot](https://twitter.com/WritePromptsBot)
--- main.py
+++ main.py
@@ -2,12 +2,15 @@
 
 import logging
 import os
+import time
 
 import tweepy
 
 logging.basicConfig()
 LOGGER = logging.getLogger("WritePromptsBot logger")
 LOGGER.setLevel(logging.DEBUG)
+
+INTERVAL = 60 * 60 * 8  # tweet every 8 hours
 
 try:
     from secret import CONSUMER_KEY, CONSUMER_SECRET, KEY, SECRET
@@ -18,13 +21,22 @@ except ImportError:
     SECRET = os.environ.get("TWITTER_SECRET")
 
 
+def tweet(twitter):
+    """Send a new tweet to the Twitter account."""
+    twitter.update_status("Hello world!")
+
+
 def main():
     """Main control flow function."""
     auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
     auth.set_access_token(KEY, SECRET)
     api = tweepy.API(auth)
 
-    LOGGER.info("Welcome {}".format(api.me().screen_name))
+    LOGGER.info("Logged in successfully as {}".format(api.me().screen_name))
+
+    while True:
+        tweet(api)
+        time.sleep(INTERVAL)
 
 
 if __name__ == "__main__":
